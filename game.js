87f98abf2cc857c7d765c251418f9789ed20b72e
Grant Register <register.grant@gmail.com>
2022-06-15 00:34:49 -0600

    added a json

--- game.js
+++ game.js
@@ -10,64 +10,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Which two teams are playing in this year's NBA Finals?",
-        choice1: "Golden State & Boston",
-        choice2: "New York & Los Angeles",
-        choice3: "Miami & Dallas",
-        choice4: "Orlando & Orlando",
-        answer: 1
-    },
-    {
-        question: "Which two teams are playing in this year's NHL Finals?",
-        choice1: "Montreal & Boston",
-        choice2: "New York & Edmonton",
-        choice3: "Tampa Bay & Colorado",
-        choice4: "San Jose & Minnesota",
-        answer: 3
-    },
-    {
-        question: "Which two teams played in this year's Super Bowl?",
-        choice1: "Tampa Bay & Kansas City",
-        choice2: "Los Angeles & Cincinnati",
-        choice3: "Miami & Dallas",
-        choice4: "Atlanta & New Orleans",
-        answer: 2
-    },
-    {
-        question: "Which two teams played in last year's World Series?",
-        choice1: "St. Louis & Kansas City",
-        choice2: "Thank goodness it wasn't the Red Sox",
-        choice3: "Tampa Bay & Los Angeles",
-        choice4: "Atlanta & Houston",
-        answer: 4
-    },
-    {
-        question: "Which four teams made it to this year's Final Four?",
-        choice1: "Duke, UNC, Kansas, Villanova",
-        choice2: "Duke, UNC, Kansas, Arizona",
-        choice3: "UNC, Kansas, Villanova, Auburn",
-        choice4: "Gators, Florida, UF, Florida Gators",
-        answer: 1
-    },
-    {
-        question: "Which two teams played in last year's College Football Championship Game?",
-        choice1: "Florida & Miami",
-        choice2: "Georgia & Oklahoma",
-        choice3: "Alabama & Clemson",
-        choice4: "Alabama & Georgia",
-        answer: 4
-    },
-    {
-        question: "Which state is clearly the best at sports?",
-        choice1: "Florida",
-        choice2: "New York",
-        choice3: "California",
-        choice4: "Texas",
-        answer: 1
-    },
-]
+let questions = []; 
+
+fetch("questions.json")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions);
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    })
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
@@ -134,5 +90,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 };
-
-startGame();
