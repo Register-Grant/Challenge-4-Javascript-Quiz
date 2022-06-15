e6352e37052a9398f7aa6ad5c7f1cd05342570be
Grant Register <register.grant@gmail.com>
2022-06-14 23:52:33 -0600

    saved high scores to local storage

--- end.js
+++ end.js
@@ -1,14 +1,28 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
-    console.log(username.value);
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 })
 
 saveHighScore = e => {
     console.log('i clicked the save button');
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 }
